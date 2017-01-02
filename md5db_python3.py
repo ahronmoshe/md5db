561dc636fce46e8257e4823261771c70a374c792
ahronmoshe <ahronmoshe1@gmail.com>
2017-01-02 22:09:36 +0200

    Update md5db_python3.py

--- md5db_python3.py
+++ md5db_python3.py
@@ -2,14 +2,13 @@ import itertools
 import hashlib
 
 c='qaz'
-fo = open("crackmd5_of_p3.txt","w+",encoding='utf-8')
+fo = open("crackmd5_of_p3.txt","w+")
 
 for i in range(1,4):
-     print (i);
+     print ("strat with "+str(i)+" combination");
      f= list(map(''.join,itertools.product(c, repeat=3)))
      for j in range(len (f)):
           fo.write(hashlib.md5(str(f[j]).encode('utf-8')).hexdigest()+" "+f[j]+"\n")
           fo.write(" ");
-
-print (len (f))
 fo.close();
+
